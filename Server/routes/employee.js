a90cc19667afaaba09879506580ef50c71784ce2
Debisa07 <debisaabebesa@gmail.com>
2023-12-30 19:52:40 +0300

    postbyid

--- Server/routes/employee.js
+++ Server/routes/employee.js
@@ -59,4 +59,24 @@ router.get("/:id", async (req, res) => {
 });
 
 
-module.exports = router;
+router.post("/:id", async (req, res) => {
+  const employeeId = req.params.id;
+  const updatedEmployeeData = req.body;
+
+  try {
+    const updatedEmployee = await models.EmployeeModel.findByIdAndUpdate(
+      employeeId,
+      updatedEmployeeData,
+      { new: true }
+    );
+
+    if (updatedEmployee) {
+      res.status(200).json(updatedEmployee);
+    } else {
+      res.status(404).json({ error: "Employee not found" });
+    }
+  } catch (error) {
+    console.error("Error updating employee:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
